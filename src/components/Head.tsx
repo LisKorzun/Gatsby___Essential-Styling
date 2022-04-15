f8b8b4659327d48fde24d951959ea6b6e429c041
akorzun <i32Middle>
2022-04-15 21:28:17 +0300

    check font load with helmet

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,12 +1,6 @@
 import React, { FC } from 'react'
 import { Helmet } from 'react-helmet'
 
-// import ComfortaaRegular from '../assets/fonts/Comfortaa/Comfortaa-Regular.ttf'
-// import ComfortaaBold from '../assets/fonts/Comfortaa/Comfortaa-Bold.ttf'
-// import ComfortaaSemiBold from '../assets/fonts/Comfortaa/Comfortaa-SemiBold.ttf'
-// import ComfortaaLight from '../assets/fonts/Comfortaa/Comfortaa-Light.ttf'
-// import ComfortaaMedium from '../assets/fonts/Comfortaa/Comfortaa-Medium.ttf'
-
 const Head: FC = () => (
   <Helmet title="foo bar" defer={false}>
     {/*<link*/}
@@ -16,7 +10,7 @@ const Head: FC = () => (
     {/*  type="font/ttf"*/}
     {/*  crossOrigin="anonymous"*/}
     {/*/>*/}
-    {/*<link rel="preload" as="font" href="/fonts/Comfortaa/Comfortaa-Bold.ttf" type="font/ttf" crossOrigin="anonymous" />*/}
+    <link rel="preload" as="font" href="/fonts/Comfortaa/Comfortaa-Bold.ttf" type="font/ttf" crossOrigin="anonymous" />
     {/*<link rel="preload" as="font" href={ComfortaaSemiBold} type="font/ttf" crossOrigin="anonymous" />*/}
     {/*<link rel="preload" as="font" href={ComfortaaLight} type="font/ttf" crossOrigin="anonymous" />*/}
     {/*<link rel="preload" as="font" href={ComfortaaMedium} type="font/ttf" crossOrigin="anonymous" />*/}
