b6be128fbee1dfe6d7fe39ed7fb19753fef4f129
akorzun <i32Middle>
2022-04-15 21:31:20 +0300

    check font load with helmet

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,13 +3,13 @@ import { Helmet } from 'react-helmet'
 
 const Head: FC = () => (
   <Helmet title="foo bar" defer={false}>
-    {/*<link*/}
-    {/*  rel="preload"*/}
-    {/*  as="font"*/}
-    {/*  href="/fonts/Comfortaa/Comfortaa-SemiBold.ttf"*/}
-    {/*  type="font/ttf"*/}
-    {/*  crossOrigin="anonymous"*/}
-    {/*/>*/}
+    <link
+      rel="preload"
+      as="font"
+      href="/fonts/Comfortaa/Comfortaa-Regular.ttf"
+      type="font/ttf"
+      crossOrigin="anonymous"
+    />
     <link rel="preload" as="font" href="/fonts/Comfortaa/Comfortaa-Bold.ttf" type="font/ttf" crossOrigin="anonymous" />
     {/*<link rel="preload" as="font" href={ComfortaaSemiBold} type="font/ttf" crossOrigin="anonymous" />*/}
     {/*<link rel="preload" as="font" href={ComfortaaLight} type="font/ttf" crossOrigin="anonymous" />*/}
