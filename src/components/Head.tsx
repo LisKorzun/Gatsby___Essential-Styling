e9cfcb0925f8c043f76424ee3aa153e09f9b0c82
akorzun <i32Middle>
2022-04-18 17:53:29 +0300

    check font load

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,15 +1,11 @@
 import React, { FC } from 'react'
 import { Helmet } from 'react-helmet'
 
+import ComfortaaRegular from '../assets/fonts/Comfortaa/Comfortaa-Regular.ttf'
+
 const Head: FC = () => (
   <Helmet title="foo bar" defer={false}>
-    {/*<link*/}
-    {/*  rel="preload"*/}
-    {/*  as="font"*/}
-    {/*  href="/fonts/Comfortaa/Comfortaa-Regular.ttf"*/}
-    {/*  type="font/ttf"*/}
-    {/*  crossOrigin="anonymous"*/}
-    {/*/>*/}
+    <link rel="preload" as="font" href={ComfortaaRegular} type="font/ttf" crossOrigin="anonymous" />
     <link rel="preload" as="font" href="/fonts/Comfortaa/Comfortaa-Bold.ttf" type="font/ttf" crossOrigin="anonymous" />
   </Helmet>
 )
--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,10 @@
 import { css } from 'styled-components'
 
 import ComfortaaRegular from '../assets/fonts/Comfortaa/Comfortaa-Regular.ttf'
+import ComfortaaBold from '../assets/fonts/Comfortaa/Comfortaa-Bold.ttf'
+import ComfortaaSemiBold from '../assets/fonts/Comfortaa/Comfortaa-SemiBold.ttf'
+import ComfortaaLight from '../assets/fonts/Comfortaa/Comfortaa-Light.ttf'
+import ComfortaaMedium from '../assets/fonts/Comfortaa/Comfortaa-Medium.ttf'
 
 const fonts = css`
   @font-face {
@@ -8,35 +12,35 @@ const fonts = css`
     src: url(${ComfortaaRegular}) format('truetype');
     font-weight: 400;
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
   }
   @font-face {
     font-family: 'Comfortaa';
-    src: url('/fonts/Comfortaa/Comfortaa-Light.ttf') format('truetype');
+    src: url(${ComfortaaLight}) format('truetype');
     font-weight: 300;
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
   }
   @font-face {
     font-family: 'Comfortaa';
-    src: url('/fonts/Comfortaa/Comfortaa-Medium.ttf') format('truetype');
+    src: url(${ComfortaaMedium}) format('truetype');
     font-weight: 500;
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
   }
   @font-face {
     font-family: 'Comfortaa';
-    src: url('/fonts/Comfortaa/Comfortaa-SemiBold.ttf') format('truetype');
+    src: url(${ComfortaaSemiBold}) format('truetype');
     font-weight: 600;
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
   }
   @font-face {
     font-family: 'Comfortaa';
-    src: url('/fonts/Comfortaa/Comfortaa-Bold.ttf') format('truetype');
+    src: url(${ComfortaaBold}) format('truetype');
     font-weight: 700;
     font-style: normal;
-    font-display: auto;
+    font-display: swap;
   }
 `
 
