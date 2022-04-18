cfff517eecbcfff28348f9c8eb968ede95120538
akorzun <i32Middle>
2022-04-18 18:09:19 +0300

    check font load

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,7 +1,7 @@
 import { css } from 'styled-components'
 
 import ComfortaaRegular from '../assets/fonts/Comfortaa/Comfortaa-Regular.ttf'
-// import ComfortaaBold from '../assets/fonts/Comfortaa/Comfortaa-Bold.ttf'
+import ComfortaaBold from '../assets/fonts/Comfortaa/Comfortaa-Bold.ttf'
 import ComfortaaSemiBold from '../assets/fonts/Comfortaa/Comfortaa-SemiBold.ttf'
 import ComfortaaLight from '../assets/fonts/Comfortaa/Comfortaa-Light.ttf'
 import ComfortaaMedium from '../assets/fonts/Comfortaa/Comfortaa-Medium.ttf'
@@ -37,7 +37,8 @@ const fonts = css`
   }
   @font-face {
     font-family: 'Comfortaa';
-    src: url('/fonts/Comfortaa/Comfortaa-Bold.ttf') format('truetype');
+    //src: url('/fonts/Comfortaa/Comfortaa-Bold.ttf') format('truetype');
+    src: url(${ComfortaaBold}) format('truetype');
     font-weight: 700;
     font-style: normal;
     font-display: swap;
