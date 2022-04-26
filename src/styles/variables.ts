5f30dec5b6f797379f8989fbc2dde057623f26af
akorzun <i32Middle>
2022-04-26 17:13:43 +0300

    check global styles

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -3,6 +3,9 @@ import { css } from 'styled-components'
 const variables = css`
   :root {
     --font-sans: 'Comfortaa', -apple-system, system-ui, sans-serif;
+    --color-text: #1b263b;
+    --color-background: #fff;
+    --color-primary: #d23669;
   }
 `
 
