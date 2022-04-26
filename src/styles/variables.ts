419bc31add04de6551821df47da4b908cf4a0a49
akorzun <i32Middle>
2022-04-26 17:19:54 +0300

    check global styles

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -3,9 +3,6 @@ import { css } from 'styled-components'
 const variables = css`
   :root {
     --font-sans: 'Comfortaa', -apple-system, system-ui, sans-serif;
-    --color-text: #1b263b;
-    --color-background: #fff;
-    --color-primary: #d23669;
   }
 `
 
--- src/utils/colorMode.ts
+++ src/utils/colorMode.ts
@@ -45,5 +45,5 @@ export const getFallBackColorStyles = () => {
     ''
   )
 
-  return `html { ${cssVariableString} }`
+  return `:root  { ${cssVariableString} }`
 }
